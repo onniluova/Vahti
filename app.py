b4d728657f36131da54312568db8373f29539ba3
onboni <onni.luova@gmail.com>
2025-12-19 16:26:58 +0200

    feat: implement routes for endpoint creation (addEndpoint)  and running statistics on a saved endpoint (runUrl)

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ import psycopg2
 from psycopg2.extras import RealDictCursor
 from werkzeug.security import generate_password_hash, check_password_hash
 from auth import createToken
+import requests
+import time
+from datetime import datetime, UTC
 
 load_dotenv()
 
@@ -99,6 +102,92 @@ def login():
             }), 200
     
     return jsonify({"error": "Invalid username or password"}), 401
+
+@app.route('/addEndpoint', methods=['POST'])
+def addEndpoint():
+    data = request.get_json()
+
+    user_id = data.get("user_id")
+    url = data.get('url')
+    name = data.get('name')
+
+    if not user_id or not url:
+        return jsonify({"error": "Missing user_id or url"}), 400
+
+    try:
+
+        conn = get_db_connection()
+        cur = conn.cursor()
+
+        query=("""
+        INSERT INTO endpoints (user_id, url, name)
+        VALUES (%s, %s, %s)
+        RETURNING id;
+        """)
+
+        cur.execute(query, (user_id, url, name))
+
+        check = cur.fetchone()[0]
+
+        conn.commit()
+        
+        cur.close()
+        conn.close()
+
+        return jsonify({
+            "message": "Created successfully",
+            "check_id": check,
+        }), 201
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+@app.route('/runUrl', methods=['POST'])
+def runUrl():
+    data = request.get_json()
+
+    url = data.get('url')
+    endpoint_id = data.get('endpoint_id')
+
+    if not url or not endpoint_id:
+        return jsonify({"error": "Missing url or endpoint_id"}), 400
+
+    try:
+        start_time = time.time()
+
+        response = requests.get(url, timeout=10)
+
+        # calculating the metrics
+        end_time = time.time()
+        latency_ms = int((end_time - start_time) * 1000)
+        status_code = response.status_code
+        is_up = 200 <= status_code < 400
+        checked_at = datetime.now(UTC)
+
+        conn = get_db_connection()
+        cur = conn.cursor()
+
+        query = ("""
+        INSERT INTO checks (endpoint_id, status_code, latency_ms, is_up, checked_at)
+        VALUES (%s, %s, %s, %s, %s)
+        RETURNING id;
+        """)
+        cur.execute(query, (endpoint_id, status_code, latency_ms, is_up, checked_at))
+
+        check_id = cur.fetchone()[0]
+        conn.commit()
+
+        cur.close()
+        conn.close()
+    
+        return jsonify({
+            "message": "Check successful",
+            "check_id": check_id,
+            "status": status_code
+        }), 200
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
