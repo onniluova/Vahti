a95fa03fd4c8846b87cc09347f5947ca9f99ed99
onboni <onni.luova@gmail.com>
2025-12-22 08:38:50 +0200

    fix: add safe checking for db url

--- app/db_conn.py
+++ app/db_conn.py
@@ -4,14 +4,17 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DB_CONFIG = {
-    "dbname": os.getenv("DB_NAME"),
-    "user": os.getenv("DB_USER"),
-    "password": os.getenv("DB_PASSWORD"),
-    "host": "localhost"
-}
-
 def get_db_connection():
+
+    url = os.getenv("DATABASE_URL")
+
+    if not url:
+        dbname = os.getenv("DB_NAME")
+        user = os.getenv("DB_USER")
+        password = os.getenv("DB_PASSWORD")
+        host = "localhost"
+        url = f"postgresql://{user}:{password}@{host}/{dbname}"
+
     if url and url.startswith("postgres://"):
         url = url.replace("postgres://", "postgresql://", 1)
 
