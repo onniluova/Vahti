74d7e3f985ac6baa503e32b70e2fcc6e44cc4d0c
onboni <onni.luova@gmail.com>
2025-12-22 14:34:26 +0200

    fix(ui): resolve card height collapse and improve responsiveness"
    - adjusted flex and grid layouts for better mobile responsiveness.
    - adjusted font fizing for different layouts

--- frontend/src/components/AnalyticsCard.jsx
+++ frontend/src/components/AnalyticsCard.jsx
@@ -1,12 +1,28 @@
 export default function AnalyticsCard({ endpoint, liveStats }) {
     if (!endpoint) return null;
 
+    const formatDate = (isoString) => {
+    
+        if (!isoString) return "Never";
+    
+        const date = new Date(isoString);
+        
+        const day = date.getDate();
+        const month = date.getMonth() + 1;
+        const year = date.getFullYear();
+        
+        const hours = date.getHours().toString().padStart(2, '0');
+        const minutes = date.getMinutes().toString().padStart(2, '0');
+        
+        return `${day}.${month}.${year} ${hours}:${minutes}`;
+    };
+
     return (
         <li 
             className="
-                group relative flex flex-col justify-between h-24 p-3 rounded-xl
+                group relative flex flex-col justify-between h-full w-full p-5 gap-2 rounded-xl
                 bg-white/5 hover:bg-white/10 
-                border border-white/10 hover:border-emerald-500/50
+                border border-emerald-500/50 hover:border-emerald-500/50
                 transition-all duration-300 ease-out
                 hover:scale-105 hover:z-10 hover:shadow-2xl hover:shadow-emerald-900/20
                 cursor-pointer
@@ -25,35 +41,35 @@ export default function AnalyticsCard({ endpoint, liveStats }) {
                 )}
             </div>
 
-            <div className="relative h-15 w-full">
+            <div className="relative h-auto w-full gap-1">
                 
                 <span className="
-                    absolute top-0 left-0 w-full
-                    text-emerald-400/70 text-[10px] font-mono break-all line-clamp-2 leading-tight
+                    top-0 left-0 w-full
+                    text-emerald-200 text-[10px] font-mono break-all line-clamp-2 leading-tight
                     transition-all duration-300
                     group-hover:opacity-0 group-hover:translate-y-2
                 ">
-                    {endpoint.url}
                 
                     {liveStats?.is_up !== undefined && (
-                        <div className={`flex flex-col gap-1 ${liveStats?.is_up? "text-emerald-500" : "text-red-500"}`}>
+                        <div className={`flex flex-col gap-1 ${liveStats?.is_up? "text-emerald-200" : "text-red-500"}`}>
                             <ul>
-                                <li>{liveStats.is_up ? "ONLINE" : "OFFLINE"}</li>
+                                <li className="text-xs">{liveStats.is_up ? "ONLINE" : "OFFLINE"}</li>
                                 <li>Status code: {liveStats.status_code}</li>
-                                <li>Checked at: {liveStats.checked_at}</li>
+                                <li>Last check: {formatDate(liveStats.checked_at)}</li>
                             </ul>
                         </div>
                     )}
                 </span>
 
                 <div className="
-                    absolute top-0 left-0 w-full flex items-center gap-2
+                    top-0 left-0 w-full flex items-center gap-2
                     text-white text-xs font-bold uppercase tracking-wider
                     opacity-0 translate-y-2
                     transition-all duration-300
                     group-hover:opacity-100 group-hover:translate-y-0
                 ">
                     <span>See full analysis</span>
+                    {endpoint.url}
                 </div>
 
             </div>
